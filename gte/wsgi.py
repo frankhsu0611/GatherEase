28e81381a42ae2d5a5639a4adfc4ca1ff3ac9e96
wilson <b05703026@ntu.edu.tw>
2023-03-07 16:10:57 -0800

    rename project

--- gte/wsgi.py
+++ gte/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for gfg project.
+WSGI config for gte project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gfg.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gte.settings")
 
 application = get_wsgi_application()
