c3703d8d837f2833b08da4046258bf5243139660
Sunnxe <s778906@gmail.com>
2023-04-11 18:37:09 +1000

    Ticket Finished

--- authentication/views.py
+++ authentication/views.py
@@ -9,21 +9,22 @@ from .api_utils import get_events
 from django.shortcuts import get_object_or_404
 from django.template.loader import get_template
 from django.template import Context
-from xhtml2pdf import pisa
 
 # Create your views here.
+
+
 def home(request):
     user = request.user
     if user.is_authenticated:
         userProfile = UserProfile.objects.get(user=user)
         conference = userProfile.conference
         events_now, events_following = get_events(request)
-        context = {'userProfile': userProfile, 
-                   'conference': conference, 
-                   'events_now': events_now, 
+        context = {'userProfile': userProfile,
+                   'conference': conference,
+                   'events_now': events_now,
                    'events_following': events_following
                    }
-        #print(user.id, context['events_now'])
+        # print(user.id, context['events_now'])
         return render(request, 'authentication/index1.html', context)
     return render(request, 'authentication/index.html')
 
@@ -39,69 +40,76 @@ def signup(request):
         userCategory = request.POST['userCategory']
         userCountry = request.POST['userCountry']
         conferenceCode = request.POST['conferenceCode']
-        
+
         if User.objects.filter(username=username).exists():
             messages.error(request, "Username already exists")
             return redirect('home')
-        
+
         if User.objects.filter(email=email).exists():
             messages.error(request, "Email already exists")
             return redirect('home')
-        
+
         if len(username) > 10:
             messages.error(request, "Username must be under 10 characters")
             return redirect('home')
-        
+
         if pass1 != pass2:
             messages.error(request, "Passwords do not match")
             return redirect('home')
-        
+
         if not username.isalnum():
             messages.error(request, "Username must be alphanumeric")
             return redirect('home')
-        
-        if userCategory not in ['speaker', 'attendee']: # add more user categories here
-            messages.error(request, "User category must be speaker or attendee")
+
+        # add more user categories here
+        if userCategory not in ['speaker', 'attendee']:
+            messages.error(
+                request, "User category must be speaker or attendee")
             return redirect('home')
-        
-        if conferenceCode not in ['ICEASS2021', 'ICEASS2022', 'ICEASS2023']: # add more conference codes here
+
+        # add more conference codes here
+        if conferenceCode not in ['ICEASS2021', 'ICEASS2022', 'ICEASS2023']:
             messages.error(request, "Conference code is invalid")
             return redirect('home')
-        
+
         myuser = User.objects.create_user(username, email, pass1)
         myuser.first_name = fname
         myuser.last_name = lname
-        myuser.save() # save to database after updating fields
+        myuser.save()  # save to database after updating fields
         update_user_profile(request, myuser)
         messages.success(request, "Your account has been successfully created")
         return redirect("sign-in")
-    
+
     return render(request, 'authentication/sign-up.html')
+
 
 def signin(request):
     if request.method == 'POST':
         username = request.POST['username']
         pass1 = request.POST['pass1']
-        
+
         user = authenticate(username=username, password=pass1)
         if user is not None:
             login(request, user)
             fname = user.first_name
-            #return render(request, 'authentication/index.html', {'fname': fname})
+            # return render(request, 'authentication/index.html', {'fname': fname})
             return redirect('home')
         else:
-            messages.error(request, "Wrong username or password. Please try again")
-            logout(request) # logout user if they are already logged in
+            messages.error(
+                request, "Wrong username or password. Please try again")
+            logout(request)  # logout user if they are already logged in
             return redirect('sign-in')
-            
+
     return render(request, 'authentication/sign-in.html')
+
 
 def signout(request):
     logout(request)
     messages.success(request, "You have been successfully logged out")
     return redirect('home')
 
-#write a function to show agenda page and pass a http response that contains the agenda.pdf stored in media/ducoment folder
+# write a function to show agenda page and pass a http response that contains the agenda.pdf stored in media/ducoment folder
+
 
 def agenda(request):
     user = request.user
@@ -110,18 +118,10 @@ def agenda(request):
     return render(request, 'pages/agenda.html', {'conference': conference})
 
 
-
-
-
-
-
-
-
-
-
 def update_user_profile(request, user):
     user.userprofile.userCategory = request.POST['userCategory']
-    user.userprofile.conference = Conference.objects.get(conferenceCode = request.POST['conferenceCode'])
+    user.userprofile.conference = Conference.objects.get(
+        conferenceCode=request.POST['conferenceCode'])
     user.userprofile.userCountry = request.POST['userCountry']
     user.save()
 
