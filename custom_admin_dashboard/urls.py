7ae109a0c00f0ec65bfdd21616c47368e69b5b47
Sunnxe <s778906@gmail.com>
2023-05-14 22:11:14 +1000

    QR code and scanner

--- custom_admin_dashboard/urls.py
+++ custom_admin_dashboard/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('dashboard/', views.home_dashboard, name='home_dashboard'),
+    path("process_ticket/", views.process_ticket, name="process_ticket"),
     path('scanner/', views.scanner, name='scanner'),
 ]
