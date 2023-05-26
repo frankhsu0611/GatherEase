7eac274b6dfb539105f1501a41f147ea6cb97ade
wilson <b05703026@ntu.edu.tw>
2023-05-25 19:58:20 -0700

    fix static settings

--- gte/settings.py
+++ gte/settings.py
@@ -109,12 +109,11 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = "/static/"
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
 ]
 
 # Default primary key field type
